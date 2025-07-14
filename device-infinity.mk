665528717d1721300c72128996b42a53dab69f02
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2025-07-14 15:04:38 -0400

    caimito: Bringup @ProjectInfinity-X
    * Inherit PixelParts - https://github.com/Evolution-X-Devices/packages_apps_PixelParts
    
    Signed-off-by: Anthony Pyrtle <xxpyrtlexx93@gmail.com>

--- device-infinity.mk
+++ device-infinity.mk
@@ -4,7 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-infinity
+
+# Always use scudo for memory allocator
+PRODUCT_USE_SCUDO := true
+
+# Camera
+include vendor/google/camera/config.mk
+
+# Googles Face Unlock
+include vendor/google/faceunlock/config.mk
+
+# PixelParts
+include packages/apps/PixelParts/device.mk
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
